4fe95e18f476a6e6349c62dcb0ff1acbad818618
DATACOM\Bryan.Nouwens <bryan.nouwens@datacom.com.au>
2017-09-04 19:23:08 +1000

    New environment build

---
+++ TfNSW/P2P/SIT/Install-Archive-Tomcat.ps1
@@ -0,0 +1,158 @@
+Write-Output "#####################################################################################"
+Write-Output " Installing Tomcat 8.0.46"
+
+try {
+    mkdir c:\temp -ErrorAction stop
+}
+catch {
+    Write-Output " Directory not created - likely already exists"
+}
+
+#Download Installer
+Write-Output " Downloading Tomcat from media bucket"
+wget https://s3-ap-southeast-2.amazonaws.com/p2pmedia/apache-tomcat-8.0.46.exe -OutFile c:\temp\apache-tomcat-8.0.46.exe
+
+#Install Tomcat
+Set-Location c:\temp
+Write-Output " Installing Tomcat"
+.\apache-tomcat-8.0.46.exe /S /D=D:\Apache Software Foundation\Tomcat 8.0
+
+#Pause
+Start-Sleep -s 15
+
+#Set Java Options
+Push-Location "D:\Apache Software Foundation\Tomcat 8.0\bin"
+.\tomcat8 //US//Tomcat8 --JvmMs=512 --JvmMx=1024 --Startup=auto
+
+#Configure Context
+$contextContent = @("<?xml version='1.0' encoding='utf-8'?>
+<!--`r
+  Licensed to the Apache Software Foundation (ASF) under one or more`r
+  contributor license agreements.  See the NOTICE file distributed with`r
+  this work for additional information regarding copyright ownership.`r
+  The ASF licenses this file to You under the Apache License, Version 2.0`r
+  (the `"License`"); you may not use this file except in compliance with`r
+  the License.  You may obtain a copy of the License at`r
+`r
+      http://www.apache.org/licenses/LICENSE-2.0`r
+`r
+  Unless required by applicable law or agreed to in writing, software`r
+  distributed under the License is distributed on an `"AS IS`" BASIS,`r
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`r
+  See the License for the specific language governing permissions and`r
+  limitations under the License.`r
+-->`r
+<!-- The contents of this file will be loaded for each web application -->`r
+<Context>`r
+`r
+    <!-- Default set of monitored resources. If one of these changes, the    -->`r
+    <!-- web application will be reloaded.                                   -->`r
+    <WatchedResource>WEB-INF/web.xml</WatchedResource>`r
+    <WatchedResource>`${catalina.base}/conf/web.xml</WatchedResource>`r
+	<CookieProcessor className=`"org.apache.tomcat.util.http.LegacyCookieProcessor`" />`r
+	`r
+    <!-- Uncomment this to disable session persistence across Tomcat restarts -->`r
+    <!--`r
+    <Manager pathname="" />`r
+    -->`r
+`r
+    <!-- Uncomment this to enable Comet connection tacking (provides events`r
+         on session expiration as well as webapp lifecycle) -->`r
+    <!--`r
+    <Valve className=`"org.apache.catalina.valves.CometConnectionManagerValve`" />`r
+    -->`r
+</Context>`r
+")
+
+#New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\context.xml" -type file -force -value $contextContent
+$contextContent | New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\context.xml" -type file -force
+
+
+#Configure Logging.properties
+$loggingContent = @("
+# Licensed to the Apache Software Foundation (ASF) under one or more`r
+# contributor license agreements.  See the NOTICE file distributed with`r
+# this work for additional information regarding copyright ownership.`r
+# The ASF licenses this file to You under the Apache License, Version 2.0`r
+# (the `"License`"); you may not use this file except in compliance with`r
+# the License.  You may obtain a copy of the License at`r
+#`r
+#     http://www.apache.org/licenses/LICENSE-2.0`r
+#`r
+# Unless required by applicable law or agreed to in writing, software`r
+# distributed under the License is distributed on an `"AS IS`" BASIS,`r
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`r
+# See the License for the specific language governing permissions and`r
+# limitations under the License.`r
+`r
+handlers = 1catalina.org.apache.juli.AsyncFileHandler, 2localhost.org.apache.juli.AsyncFileHandler, 3manager.org.apache.juli.AsyncFileHandler, 4host-manager.org.apache.juli.AsyncFileHandler, java.util.logging.ConsoleHandler`r
+`r
+.handlers = 1catalina.org.apache.juli.AsyncFileHandler`r
+`r
+############################################################`r
+# Handler specific properties.`r
+# Describes specific configuration info for Handlers.`r
+############################################################`r
+`r
+1catalina.org.apache.juli.AsyncFileHandler.level = FINE`r
+1catalina.org.apache.juli.AsyncFileHandler.directory = `${catalina.base}/logs`r
+1catalina.org.apache.juli.AsyncFileHandler.prefix = catalina.`r
+`r
+2localhost.org.apache.juli.AsyncFileHandler.level = FINE`r
+2localhost.org.apache.juli.AsyncFileHandler.directory = `${catalina.base}/logs`r
+2localhost.org.apache.juli.AsyncFileHandler.prefix = localhost.`r
+`r
+3manager.org.apache.juli.AsyncFileHandler.level = FINE`r
+3manager.org.apache.juli.AsyncFileHandler.directory = `${catalina.base}/logs`r
+3manager.org.apache.juli.AsyncFileHandler.prefix = manager.`r
+`r
+4host-manager.org.apache.juli.AsyncFileHandler.level = FINE`r
+4host-manager.org.apache.juli.AsyncFileHandler.directory = `${catalina.base}/logs`r
+4host-manager.org.apache.juli.AsyncFileHandler.prefix = host-manager.`r
+`r
+java.util.logging.ConsoleHandler.level = FINE`r
+java.util.logging.ConsoleHandler.formatter = org.apache.juli.OneLineFormatter`r
+`r
+`r
+############################################################`r
+# Facility specific properties.`r
+# Provides extra control for each logger.`r
+############################################################`r
+`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].level = INFO`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].handlers = 2localhost.org.apache.juli.AsyncFileHandler`r
+`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].[/manager].level = INFO`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].[/manager].handlers = 3manager.org.apache.juli.AsyncFileHandler`r
+`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].[/host-manager].level = INFO`r
+org.apache.catalina.core.ContainerBase.[Catalina].[localhost].[/host-manager].handlers = 4host-manager.org.apache.juli.AsyncFileHandler`r
+`r
+# For example, set the org.apache.catalina.util.LifecycleBase logger to log`r
+# each component that extends LifecycleBase changing state:`r
+#org.apache.catalina.util.LifecycleBase.level = FINE`r
+`r
+# To see debug messages in TldLocationsCache, uncomment the following line:`r
+#org.apache.jasper.compiler.TldLocationsCache.level = FINE`r
+`r
+")
+
+#New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\logging.properties" -type file -force -value $loggingContent
+$loggingContent | New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\logging.properties" -type file -force
+
+#Configure Environment Variables
+[Environment]::SetEnvironmentVariable("AS_PREF_IP", "4", "Machine")
+
+Write-Output " Starting Tomcat"
+net start tomcat8
+
+#Configure Local Windows Firewall
+$findFWRule = Get-NetFirewallRule -DisplayName "Apache Web-In*"
+$int = 0
+foreach ($record in $findFWRule){$int++}
+
+if ($int -lt 1)
+{
+    echo "No Rule Found"
+    netsh advfirewall firewall add rule name="Apache Web-In" dir=in localport=8080 protocol=TCP action=allow
+}
---
+++ TfNSW/P2P/SIT/Install-DS-Tomcat.ps1
@@ -0,0 +1,194 @@
+Write-Output "#####################################################################################"
+Write-Output " Installing Tomcat 8.0.46"
+
+try {
+    mkdir c:\temp -ErrorAction stop
+}
+catch {
+    Write-Output " Directory not created - likely already exists"
+}
+
+#Download Installer
+Write-Output " Downloading Tomcat from media bucket"
+wget https://s3-ap-southeast-2.amazonaws.com/p2pmedia/apache-tomcat-8.0.46.exe -OutFile c:\temp\apache-tomcat-8.0.46.exe
+
+#Install Tomcat
+Set-Location c:\temp
+Write-Output " Installing Tomcat"
+.\apache-tomcat-8.0.46.exe /S /D=D:\Apache Software Foundation\Tomcat 8.0
+
+#Pause
+Start-Sleep -s 15
+
+#In case Tomcat is running - stop it
+Write-Output " Starting Tomcat"
+net stop tomcat8
+
+#Set Java Options
+Push-Location "D:\Apache Software Foundation\Tomcat 8.0\bin"
+.\tomcat8 //US//Tomcat8 --JvmMs=256 --JvmMx=1024 --Startup=auto
+
+#Configure Server.xml
+
+#Configure Server.xml
+$serverContent = @("<?xml version='1.0' encoding='utf-8'?>`r
+<!--`r
+  Licensed to the Apache Software Foundation (ASF) under one or more`r
+  contributor license agreements.  See the NOTICE file distributed with`r
+  this work for additional information regarding copyright ownership.`r
+  The ASF licenses this file to You under the Apache License, Version 2.0`r
+  (the `"License`"); you may not use this file except in compliance with`r
+  the License.  You may obtain a copy of the License at`r
+`r
+      http://www.apache.org/licenses/LICENSE-2.0`r
+`r
+  Unless required by applicable law or agreed to in writing, software`r
+  distributed under the License is distributed on an `"AS IS`" BASIS,`r
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`r
+  See the License for the specific language governing permissions and`r
+  limitations under the License.`r
+-->`r
+<!-- Note:  A `"Server`" is not itself a `"Container`", so you may not`r
+     define subcomponents such as `"Valves`" at this level.`r
+     Documentation at /docs/config/server.html`r
+ -->`r
+<Server port=`"8005`" shutdown=`"SHUTDOWN`">`r
+  <Listener className=`"org.apache.catalina.startup.VersionLoggerListener`" />`r
+  <!-- Security listener. Documentation at /docs/config/listeners.html`r
+  <Listener className=`"org.apache.catalina.security.SecurityListener`" />`r
+  -->`r
+  <!--APR library loader. Documentation at /docs/apr.html -->`r
+  <Listener className=`"org.apache.catalina.core.AprLifecycleListener`" SSLEngine=`"on`" />`r
+  <!-- Prevent memory leaks due to use of particular java/javax APIs-->`r
+  <Listener className=`"org.apache.catalina.core.JreMemoryLeakPreventionListener`" />`r
+  <Listener className=`"org.apache.catalina.mbeans.GlobalResourcesLifecycleListener`" />`r
+  <Listener className=`"org.apache.catalina.core.ThreadLocalLeakPreventionListener`" />`r
+`r
+  <!-- Global JNDI resources`r
+       Documentation at /docs/jndi-resources-howto.html`r
+  -->`r
+  <GlobalNamingResources>`r
+    <!-- Editable user database that can also be used by`r
+         UserDatabaseRealm to authenticate users`r
+    -->`r
+    <Resource name=`"UserDatabase`" auth=`"Container`"`r
+              type=`"org.apache.catalina.UserDatabase`"`r
+              description=`"User database that can be updated and saved`"`r
+              factory=`"org.apache.catalina.users.MemoryUserDatabaseFactory`"`r
+              pathname=`"conf/tomcat-users.xml`" />`r
+  </GlobalNamingResources>`r
+`r
+  <!-- A `"Service`" is a collection of one or more `"Connectors`" that share`r
+       a single `"Container`" Note:  A `"Service`" is not itself a `"Container`",`r
+       so you may not define subcomponents such as `"Valves`" at this level.`r
+       Documentation at /docs/config/service.html`r
+   -->`r
+  <Service name=`"Catalina`">`r
+`r
+    <!--The connectors can use a shared executor, you can define one or more named thread pools-->`r
+    <!--`r
+    <Executor name=`"tomcatThreadPool`" namePrefix=`"catalina-exec-`"`r
+        maxThreads=`"150`" minSpareThreads=`"4`"/>`r
+    -->`r
+`r
+`r
+    <!-- A `"Connector`" represents an endpoint by which requests are received`r
+         and responses are returned. Documentation at :`r
+         Java HTTP Connector: /docs/config/http.html (blocking & non-blocking)`r
+         Java AJP  Connector: /docs/config/ajp.html`r
+         APR (HTTP/AJP) Connector: /docs/apr.html`r
+         Define a non-SSL/TLS HTTP/1.1 Connector on port 8080`r
+    -->`r
+    <Connector port=`"8080`" protocol=`"HTTP/1.1`"`r
+               connectionTimeout=`"20000`"`r
+               redirectPort=`"8443`"`r
+               maxHttpHeaderSize=`"65536`" />`r
+    <!-- A `"Connector`" using the shared thread pool-->`r
+    <!--`r
+    <Connector executor=`"tomcatThreadPool`"`r
+               port=`"8080`" protocol=`"HTTP/1.1`"`r
+               connectionTimeout=`"20000`"`r
+               redirectPort=`"8443`" />`r
+    -->`r
+    <!-- Define a SSL/TLS HTTP/1.1 Connector on port 8443`r
+         This connector uses the NIO implementation that requires the JSSE`r
+         style configuration. When using the APR/native implementation, the`r
+         OpenSSL style configuration is required as described in the APR/native`r
+         documentation -->`r
+    <!--`r
+    <Connector port=`"8443`" protocol=`"org.apache.coyote.http11.Http11NioProtocol`"`r
+               maxThreads=`"150`" SSLEnabled=`"true`" scheme=`"https`" secure=`"true`"`r
+               clientAuth=`"false`" sslProtocol=`"TLS`" />`r
+    -->`r
+`r
+    <!-- Define an AJP 1.3 Connector on port 8009 -->`r
+    <Connector port=`"8009`" protocol=`"AJP/1.3`" redirectPort=`"8443`" />`r
+`r
+`r
+    <!-- An Engine represents the entry point (within Catalina) that processes`r
+         every request.  The Engine implementation for Tomcat stand alone`r
+         analyzes the HTTP headers included with the request, and passes them`r
+         on to the appropriate Host (virtual host).`r
+         Documentation at /docs/config/engine.html -->`r
+`r
+    <!-- You should set jvmRoute to support load-balancing via AJP ie :`r
+    <Engine name=`"Catalina`" defaultHost=`"localhost`" jvmRoute=`"jvm1`">`r
+    -->`r
+    <Engine name=`"Catalina`" defaultHost=`"localhost`">`r
+`r
+      <!--For clustering, please take a look at documentation at:`r
+          /docs/cluster-howto.html  (simple how to)`r
+          /docs/config/cluster.html (reference documentation) -->`r
+      <!--`r
+      <Cluster className=`"org.apache.catalina.ha.tcp.SimpleTcpCluster`"/>`r
+      -->`r
+`r
+      <!-- Use the LockOutRealm to prevent attempts to guess user passwords`r
+           via a brute-force attack -->`r
+      <Realm className=`"org.apache.catalina.realm.LockOutRealm`">`r
+        <!-- This Realm uses the UserDatabase configured in the global JNDI`r
+             resources under the key `"UserDatabase`".  Any edits`r
+             that are performed against this UserDatabase are immediately`r
+             available for use by the Realm.  -->`r
+        <Realm className=`"org.apache.catalina.realm.UserDatabaseRealm`"`r
+               resourceName=`"UserDatabase`"/>`r
+      </Realm>`r
+`r
+      <Host name=`"localhost`"  appBase=`"webapps`"`r
+            unpackWARs=`"true`" autoDeploy=`"true`">`r
+`r
+        <!-- SingleSignOn valve, share authentication between web applications`r
+             Documentation at: /docs/config/valve.html -->`r
+        <!--`r
+        <Valve className=`"org.apache.catalina.authenticator.SingleSignOn`" />`r
+        -->`r
+`r
+        <!-- Access log processes all example.`r
+             Documentation at: /docs/config/valve.html`r
+             Note: The pattern used is equivalent to using pattern=`"common`" -->`r
+        <Valve className=`"org.apache.catalina.valves.AccessLogValve`" directory=`"logs`"`r
+               prefix=`"localhost_access_log`" suffix=`".txt`"`r
+               pattern=`"%h %l %u %t &quot;%r&quot; %s %b`" />`r
+`r
+      </Host>`r
+    </Engine>`r
+  </Service>`r
+</Server>`r
+`r
+")
+
+#New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\context.xml" -type file -force -value $contextContent
+$serverContent | New-Item "D:\Apache Software Foundation\Tomcat 8.0\conf\server.xml" -type file -force
+
+Write-Output " Starting Tomcat"
+net start tomcat8
+
+#Configure Local Windows Firewall
+$findFWRule = Get-NetFirewallRule -DisplayName "Apache Web-In*"
+$int = 0
+foreach ($record in $findFWRule) {$int++}
+
+if ($int -lt 1) {
+    echo " Creating Firewall Rule"
+    netsh advfirewall firewall add rule name="Apache Web-In" dir=in localport=8080 protocol=TCP action=allow
+}
