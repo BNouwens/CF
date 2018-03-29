e072db9109a91a1c76c88590ffa6e0e114d485a0
DATACOM\Bryan.Nouwens <bryan.nouwens@datacom.com.au>
2018-03-29 11:40:45 +1100

    Repo Cleanup

--- Building-Blocks/AWS/Powershell/disable-trend.ps1
+++ Building-Blocks/AWS/Powershell/disable-trend.ps1
@@ -1,3 +1,10 @@
+#In Powershell
+
 Push-location "c:\Program Files\Trend Micro\Deep Security Agent"
 .\dsa_control -r
 Stop-Service ds_agent
+
+#May want to disable service so on restart it is not re-enabled
+
+
+
