23be7d436c21a16a707b3101fe0b31564a0f47d6
DATACOM\Bryan.Nouwens <bryan.nouwens@datacom.com.au>
2018-06-04 11:25:26 +1000

    Completed v1.0

--- Building-Blocks/AWS/Lambda/Check-Compliance.js
+++ Building-Blocks/AWS/Lambda/Check-Compliance.js
@@ -1,6 +1,4 @@
 var aws = require("aws-sdk");
-// var ssm = new aws.SSM();
-// var ec2 = new aws.EC2();
 
 function getInventory(results, token) {
     return new Promise((resolve, reject) => {
@@ -75,75 +73,6 @@ function getInstanceCompliance(instanceID) {
                 const instanceSummary = data['ResourceComplianceSummaryItems'];
                 summaryData = instanceSummary[0];
                 resolve(summaryData);
-                // console.log(summaryData);
-            }
-        });
-    })
-}
-
-
-
-
-function describeInstancePatchForGroup(results, token) {
-    return new Promise((resolve, reject) => {
-        var ssm = new aws.SSM();
-        var PatchGroup = process.env.PatchGroup;
-        var params = {
-            PatchGroup: PatchGroup,
-            MaxResults: 1
-        };
-        if (token) {
-            params.NextToken = token
-        }
-        ssm.describeInstancePatchStatesForPatchGroup(params, function (err, data) {
-            if (err) {
-                if (err.code === 'ThrottlingException' || err.code === 'TooManyRequestsException') {
-                    setTimeout(() => {
-                        resolve(describeInstancePatchForGroup(results, token));
-                    }, 1000);
-                } else {
-                    reject(err);
-                }
-            }
-            else {
-                if (data.NextToken) {
-                    resolve(describeInstancePatchForGroup(results.concat(data.InstancePatchStates), data.NextToken))
-                }
-                else {
-                    resolve(results.concat(data.InstancePatchStates))
-                }
-            }
-        });
-    })
-}
-
-function patchGroups(results, token) {
-    return new Promise((resolve, reject) => {
-        var ssm = new aws.SSM();
-        var params = {
-            MaxResults: 50,
-        };
-        if (token) {
-            params.NextToken = token
-        }
-        ssm.describePatchGroups(params, function (err, data) {
-            if (err) {
-                if (err.code === 'ThrottlingException' || err.code === 'TooManyRequestsException') {
-                    setTimeout(() => {
-                        resolve(patchGroups(results, token));
-                    }, 1000);
-                } else {
-                    reject(err);
-                }
-
-            }
-            else {
-                if (data.NextToken) {
-                    resolve(patchGroups(results.concat(data.Mappings[0].PatchGroup), data.NextToken))
-                }
-                else {
-                    resolve(results.concat(data.Mappings[0].PatchGroup))
-                }
             }
         });
     })
@@ -152,8 +81,6 @@ function patchGroups(results, token) {
 function sendSNS(message) {
     var sns = new aws.SNS();
     var topicArn = process.env.TopicArn;
-
-    // var subject = accountId + " - Patch Details"
     var params = {
         Message: JSON.stringify(message),
         Subject: 'Patch Details',
@@ -168,35 +95,12 @@ function sendSNS(message) {
 exports.handler = function (event, context, callback) {
     var invData;
 
-
     var err = function (err) {
         console.log(err);
     }
     var promises = [];
-    // Query AWS Account ID for report
     ACCOUNT_ID = context.invokedFunctionArn.split(":")[4];
     const accountNumber = ACCOUNT_ID;
-    instanceId = "i-0a5f610c359a70d3f";
-
-    /*    getInventory([])
-              .then(function (data) {
-                  data.forEach(value => {
-                      // console.log(JSON.stringify(value));
-                      if (value.Data['AWS:InstanceInformation'].Content[0].InstanceStatus != "Terminated") {
-                          promises.push(value.Data['AWS:InstanceInformation'].Content[0].InstanceId);
-                          promises.push(getInstanceCompliance(value.Data['AWS:InstanceInformation'].Content[0].InstanceId));
-      
-                      }
-                  });
-                  Promise.all(promises)
-                      .then(function (data) {
-                          console.log(data);
-                      })
-              }).catch(function (err) { console.log(err) });
-       */
-
-
-
     getInventory([])
         .then(function (data) {
             data.forEach(value => {
@@ -209,67 +113,18 @@ exports.handler = function (event, context, callback) {
                     // console.log(data);
                     allDetail = [];
                     data.forEach(value => {
-                        var objDetail = new Object();
-                        objDetail.Account = accountNumber;
-                        objDetail.instanceId = value.ResourceId;
-                        objDetail.Status = value.Status;
-                        objDetail.InstalledCount = value.CompliantSummary.CompliantCount;
-                        objDetail.MissingCount = value.NonCompliantSummary.NonCompliantCount;
-                        allDetail.push(objDetail);
+                        if (value != undefined) {
+                            var objDetail = new Object();
+                            objDetail.Account = accountNumber;
+                            objDetail.instanceId = value.ResourceId;
+                            objDetail.Status = value.Status;
+                            objDetail.InstalledCount = value.CompliantSummary.CompliantCount;
+                            objDetail.MissingCount = value.NonCompliantSummary.NonCompliantCount;
+                            allDetail.push(objDetail);
+                        }
                     });
                     console.log(allDetail);
                     sendSNS(allDetail);
-
                 })
         }).catch(function (err) { console.log(err) });
-
-
-    /*     
-getInstanceCompliance(instanceId)
-    .then(function (data) {
-        var allDetail = []
-
-        var objDetail = new Object();
-        objDetail.Account = accountNumber;
-        objDetail.instanceId = data.ResourceId;
-        objDetail.Status = data.Status;
-        objDetail.InstalledCount = data.CompliantSummary.CompliantCount;
-        objDetail.MissingCount = data.NonCompliantSummary.NonCompliantCount;
-        allDetail.push(objDetail);
-        promises.push(objDetail);
-        Promise.all(promises);
-        console.log(promises);
-        // sendSMS(promises);
-    }).catch(function (err) { console.log(err) });
-
-*/
-    /* 
-    
-        describeInstancePatchForGroup([])
-            .then(function (data) {
-                var allDetail = []
-    
-                data.forEach(value => {
-                    var objDetail = new Object();
-                    objDetail.Account = accountNumber;
-                    objDetail.InstanceId = value.InstanceId;
-                    objDetail.PatchGroup = value.PatchGroup;
-                    objDetail.InstalledCount = value.InstalledCount;
-                    objDetail.MissingCount = value.MissingCount;
-    
-                    if (value.MissingCount > 0) {
-                        objDetail.Compliant = "False";
-                    }
-                    else { objDetail.Compliant = "True"; }
-                    allDetail.push(objDetail);
-                    promises.push(objDetail);
-                });
-                Promise.all(promises);
-                console.log(promises);
-                sendSMS(promises);
-            }).catch(function (err) { console.log(err) });
-    
-     */
-
-
 };
