223bd510cd75bb7bf313292c553d03f2e4e3546e
DATACOM\Bryan.Nouwens <bryan.nouwens@datacom.com.au>
2018-06-05 08:28:45 +1000

    Stop Start Instance scripts vpc -9 and -1 cloudformations

---
+++ Building-Blocks/AWS/Lambda/Stop-Instances.js
@@ -0,0 +1,46 @@
+var aws = require("aws-sdk");
+
+function stopEc2Instance(instanceParams) {
+    var ec2 = new aws.EC2();
+    ec2.stopInstances(instanceParams, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
+}
+
+exports.handler = function (event, context, callback) {
+    var ec2 = new aws.EC2();
+    var instanceSearch = {
+        Filters: [
+            {
+                Name: 'tag:AutoOnOff',
+                Values: [
+                    'yes'
+                ]
+            },
+        ],
+        DryRun: false,
+        MaxResults: 50,
+    };
+    ec2.describeInstances(instanceSearch).promise()
+        .then((response) => {
+            const promises = [];
+            response.Reservations.forEach(value => {
+                var params = {
+                    InstanceIds: [
+                        value.Instances[0].InstanceId
+                    ],
+                    DryRun: false
+                };
+                promises.push(stopEc2Instance(params));
+            })
+            return Promise.all(promises)
+        }).then(() => {
+            console.log('Im all finished');
+            callback(null);
+        }).catch(err => {
+            console.log('Something went really wrong')
+            callback(err);
+        })
+
+};
